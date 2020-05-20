3c7741668d5502c6adb0149bf57cb01684bba552
lihao <lihao@loyi.cn>
2020-05-20 13:10:13 +0800

    [ADD]测试

---
+++ src/main/java/Arg.java
@@ -0,0 +1,34 @@
+public class Arg {
+    private String rowValue;
+    private String tag;
+    private String type;
+
+    public Arg(Schema schema, String tag, String rowValue) {
+        this.tag = tag.replace("-","");
+        this.rowValue = rowValue;
+        this.type = schema.specOf(this.tag);
+    }
+
+    public String getRowValue() {
+        return rowValue;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public Object value() {
+        if (type.equals("boolean")){
+            return Boolean.valueOf(rowValue);
+        }
+        if (type.equals("integer")){
+            return Integer.valueOf(rowValue);
+        }
+        return rowValue;
+
+    }
+}
